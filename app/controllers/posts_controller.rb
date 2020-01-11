f0452ff1a7062fdedc115946bc759ad274eb1dd4
John Louis Rumingan <jmrumingan@gmail.com>
2020-01-11 11:25:09 -0500

    rails form_tag

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,17 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
   
+  def new 
+
+    #just loads the new post page
+  end 
+
+  def create #this is the Sinatra post method with redirect 
+
+    @post = Post.create(title: params[:post][:title], description: params[:post][:description])
+    #note the nested arrays format that follows the form post layout 
+    redirect_to posts_path #without specifying anything, posts_path goes to index 
+              #note the difference with pluralization: custom_post_path (singular)
+  end 
+  
 end
